a957a76ec815e9ef1385a608450f6db6c8fc7ff7
Syarief A <syariefarifin107@gmail.com>
2019-08-22 14:41:03 +0700

    akhirnya selesai

---
+++ exercise-7.js
@@ -0,0 +1,13 @@
+function hapusSimbol(str) {
+    
+    var hasilnya = str.match(/[0-9a-z]+/gi).join("")
+
+    return hasilnya;
+}
+  
+  // TEST CASES
+  console.log(hapusSimbol('test%$4aa')); // test4aa
+  console.log(hapusSimbol('devel0p3r s3j@@ati')); // devel0p3rs3jati
+  console.log(hapusSimbol('ma@#k!an~')); // makan
+  console.log(hapusSimbol('coding')); // coding
+  console.log(hapusSimbol('1+3-5*2=100')); // 1352100
---
+++ exercise-8.js
@@ -0,0 +1,12 @@
+function cariPelaku(str) {
+
+    var hasilnya = str.match(/abc/gi).length;
+    return hasilnya;
+}
+  
+  // TEST CASES
+  console.log(cariPelaku('mabcvabc')); // 2
+  console.log(cariPelaku('abcdabdc')); // 1
+  console.log(cariPelaku('bcabcac')); // 1
+  console.log(cariPelaku('abcabcabc')); // 3
+  console.log(cariPelaku('babcbacabc')); // 2
